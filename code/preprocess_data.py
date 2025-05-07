7729355b2e1ea9950babc3af945ab6a3500e129c
dancejod <dancejod@natur.cuni.cz>
2025-05-07 14:24:35 +0200

    add docstrings

--- code/preprocess_data.py
+++ code/preprocess_data.py
@@ -3,25 +3,28 @@ import pandas as pd
 from scipy.spatial import distance
 
 def get_buildings(path_to_buildings):
+    """Returns building centroids with their cluster assignment at various levels of aggregation, and a list of these levels."""
+    #Read buildings dataset
     buildings = gpd.read_parquet(path_to_buildings)
-    
+    #Read cluster mapping
     final_clusters = pd.read_parquet(
         "/data/uscuni-ulce/processed_data/clusters/cluster_mapping_v10.pq")
     
     level_columns = []
-    
+    #Remap building centroids to clusters on levels in 'final_clusters' (max level 7)
     for level in final_clusters.columns:
         buildings[f"level_{level}"] = buildings.final_without_noise.map(
             final_clusters[level]
         )
         level_columns.append(f"level_{level}")
-    
+    #Rename lowest level to level 7
     level_7 = buildings.pop("final_without_noise")
     buildings["level_7"] = level_7
     level_columns.append("level_7")
     return buildings, level_columns
 
 def get_building_counts_per_region(buildings, boundary, level="level_4"):
+    """Returns count table of buildings per region in a specified layer."""
     joined = gpd.sjoin(buildings, boundary, how="left", predicate="within")
     grouped = joined.groupby("NAME")[level].value_counts()
     cnt_table = pd.DataFrame(grouped.unstack().fillna(0).T)
@@ -29,6 +32,7 @@ def get_building_counts_per_region(buildings, boundary, level="level_4"):
     return cnt_table
 
 def normalize_building_counts(count_table):
+    """Returns normalized count table."""
     region_building_sums = count_table.sum(axis=0)
     cnt_table_norm = count_table.div(region_building_sums, axis=1)
     return cnt_table_norm.T
